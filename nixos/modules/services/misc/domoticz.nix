382231a1fb3631e2786cae219fea6b1e3871af52
Ed Cragg <drq.11235@gmail.com>
2020-04-26 11:58:38 +0100

    More basic not-module

--- nixos/modules/services/misc/domoticz.nix
+++ nixos/modules/services/misc/domoticz.nix
@@ -1,47 +1,28 @@
-{ stdenv, fetchurl, cmake, pkgconfig, darwin
-, openexr, zlib, imagemagick, libGLU, libGL, freeglut, fftwFloat
-, fftw, gsl, libexif, perl, opencv, qt5, netpbm
-}:
+{ stdenv, fetchzip, cmake}:
 
 stdenv.mkDerivation rec {
   pname = "demoticz";
   version = "2020.1";
 
-  src = fetchurl {
-    url = "mirror://sourceforge/${pname}/${version}/${pname}-${version}.tgz";
-    sha256 = "04rlb705gmdiphcybf9dyr0d5lla2cfs3c308zz37x0vwi445six";
+  src = fetchzip {
+    url = "https://github.com/anope/anope/archive/${version}.tar.gz";
+    sha256 = "0mq4ww7nl2n4mdyaqby35ih9a3iqcdq5wj7hiavabihla0ivi8gd";
   };
 
-  outputs = [ "out" "dev" "man"];
+  nativeBuildInputs = [ cmake ];
 
-  cmakeFlags = [ "-DWITH_MATLAB=false" ];
-
-  preConfigure = ''
-    rm cmake/FindNETPBM.cmake
-    echo "SET(NETPBM_LIBRARY `find ${stdenv.lib.getLib netpbm} -name "*.${stdenv.hostPlatform.extensions.sharedLibrary}*" -type f`)" >> cmake/FindNETPBM.cmake
-    echo "SET(NETPBM_LIBRARIES `find ${stdenv.lib.getLib netpbm} -name "*.${stdenv.hostPlatform.extensions.sharedLibrary}*" -type f`)" >> cmake/FindNETPBM.cmake
-    echo "SET(NETPBM_INCLUDE_DIR ${stdenv.lib.getDev netpbm}/include/netpbm)" >> cmake/FindNETPBM.cmake
-    echo "INCLUDE(FindPackageHandleStandardArgs)" >> cmake/FindNETPBM.cmake
-    echo "FIND_PACKAGE_HANDLE_STANDARD_ARGS(NETPBM DEFAULT_MSG NETPBM_LIBRARY NETPBM_INCLUDE_DIR)" >> cmake/FindNETPBM.cmake
-  '';
-
-  nativeBuildInputs = [ cmake pkgconfig ];
-  buildInputs = [
-    openexr zlib imagemagick fftwFloat
-    fftw gsl libexif perl opencv qt5.qtbase netpbm
-  ] ++ (if stdenv.isDarwin then (with darwin.apple_sdk.frameworks; [
-    OpenGL GLUT
-  ]) else [
-    libGLU libGL freeglut
-  ]);
-
-  patches = [ ./threads.patch ./pfstools.patch ./pfsalign.patch ];
+  cmakeFlags = [
+  ];
 
   meta = with stdenv.lib; {
-    homepage = "http://pfstools.sourceforge.net/";
-    description = "Toolkit for manipulation of HDR images";
-    platforms = platforms.linux;
-    license = licenses.lgpl2;
-    maintainers = [ maintainers.juliendehos ];
+    description = "Domoticz home automation system";
+    longDescription = ''
+      Domoticz is a home automation system that lets you monitor and configure
+      various devices like: lights, switches, various sensors/meters like
+      temperature, rain, wind, UV, electra, gas, water and much more
+    '';
+    homepage = "https://www.domoticz.com/";
+    license = licenses.gpl3;
+    platforms = platforms.all;
   };
 }
