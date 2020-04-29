edd9a4577abfb0ed2305cf365db0da65ffddce8f
Ed Cragg <drq.11235@gmail.com>
2020-04-30 00:34:45 +0100

    Get submodules

--- pkgs/servers/domoticz/default.nix
+++ pkgs/servers/domoticz/default.nix
@@ -1,5 +1,5 @@
 { stdenv,
-  fetchzip,
+  fetchgit,
   cmake,
   python3,
   openssl,
@@ -19,9 +19,12 @@ stdenv.mkDerivation rec {
   name = "domoticz";
   version = "2020.2";
 
-  src = fetchzip {
-    url = "https://github.com/domoticz/domoticz/archive/${version}.tar.gz";
-    sha256 = "1b4pkw9qp7f5r995vm4xdnpbwi9vxjyzbnk63bmy1xkvbhshm0g3";
+  src = fetchgit {
+    url = "https://github.com/domoticz/domoticz.git";
+    rev = "aad65002eea99b8336cefd773e7422f0a7f1873f";
+    sha256 = "06vi9ambn285z8v7n9h85lcx77nvw1s9h78719fll5czzmm250xx";
+    deepClone = true;
+    fetchSubmodules = true;
   };
 
   enableParallelBuilding = true;
