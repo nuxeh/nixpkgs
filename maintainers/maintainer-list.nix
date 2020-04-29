0b314436afb0466bc148c81b96b58d716bb5aece
Ed Cragg <ed.cragg@eipi.xyz>
2020-04-29 23:25:25 +0100

    maintainers: add edcragg

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -2158,6 +2158,12 @@
     githubId = 984691;
     name = "Evan Danaher";
   };
+  edcragg = {
+    email = "ed.cragg@eipi.xyz";
+    github = "nuxeh";
+    githubId = 1516017;
+    name = "Ed Cragg";
+  };
   edef = {
     email = "edef@edef.eu";
     github = "edef1c";
