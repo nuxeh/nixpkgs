7b2ba02cfbed3f7722eb4a24a7f60176af4bf696
Ed Cragg <drq.11235@gmail.com>
2020-05-05 23:50:38 +0100

    fixup! pi-hole-ftl: init at v4.3.1

--- pkgs/tools/networking/pi-hole-ftl/default.nix
+++ pkgs/tools/networking/pi-hole-ftl/default.nix
@@ -1,16 +1,29 @@
 { stdenv,
   fetchzip,
+  fetchgit,
   nettle,
   gmp
 }:
 
-stdenv.mkDerivation rec {
-  name = "pi-hole-ftl";
-  version = "v4.3.1";
+let
 
-  src = fetchzip {
-    url = "https://github.com/pi-hole/FTL/archive/${version}.tar.gz";
-    sha256 = "05bvwmfqg52ic7f95d419hnqnxlixnqzx2fi93ki3axxz1g56l6p";
+  # It's tricky to have .git present at build time, but the Makefile shells
+  # out to git expecting to fill the following values, which need to be
+  # set, or we get build errors.
+  gitHash = "01332f610120f2611cd621372f66c18375033407";
+  gitBranch = "release/v5.0";
+  gitVersion = "v4.3.1-820-g01332f6";
+  gitDate = "2020-05-05 21:19:48 +0200";
+  gitTag = "v4.3.1";
+
+in stdenv.mkDerivation rec {
+  name = "pihole-ftl";
+  version = "v5.0";
+
+  src = fetchgit {
+    url = "https://github.com/nuxeh/FTL.git";
+    rev = "${gitHash}";
+    sha256 = "0zbpkgy5pcwi6z1hsnf2y44g12wm8ja39nvb5iqshv3zip7nahfj";
   };
 
   enableParallelBuilding = true;
@@ -20,10 +33,16 @@ stdenv.mkDerivation rec {
     gmp
   ];
 
-  # disable static linking
   preBuild = ''
+    # disable static linking
     makeFlagsArray+=(LIBS="-pthread -lrt -lhogweed -lgmp -lnettle")
-'';
+    # override git variables
+    makeFlagsArray+=(GIT_HASH="${gitHash}")
+    makeFlagsArray+=(GIT_BRANCH="${gitBranch}")
+    makeFlagsArray+=(GIT_VERSION="${gitVersion}")
+    makeFlagsArray+=(GIT_DATE="${gitDate}")
+    makeFlagsArray+=(GIT_TAG="${gitTag}")
+  '';
 
   installPhase = ''
     mkdir -p $out/bin
@@ -34,7 +53,9 @@ stdenv.mkDerivation rec {
     description = "Pi-hole FTL engine";
     homepage = "https://github.com/pi-hole/FTL";
     license = licenses.eupl11; ## TODO eupl12
-    maintainers = with maintainers; [ edcragg ];
+    #maintainers = with maintainers; [ edcragg ];
     platforms = platforms.all;
   };
 }
+# https://github.com/pi-hole/FTL/blob/master/Makefile
+# /sbin/setcap CAP_NET_BIND_SERVICE,CAP_NET_RAW,CAP_NET_ADMIN+eip $(prefix)/bin/pihole-FTL
