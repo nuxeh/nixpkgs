4e5f0ca4b33180243f2c6e9bd25951ce3ed4a1b6
Martin Weinelt <hexa@darmstadt.ccc.de>
2020-11-29 03:06:39 +0100

    nixos/url-bot-rs: init
    url-bot-rs is a minimal IRC bot that looks up URLs posted on IRC and
    extracts title and other metadata and relays it back to the IRC channel
    it was originally mentioned.

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -902,6 +902,7 @@
   ./services/networking/unifi.nix
   ./services/video/unifi-video.nix
   ./services/video/rtsp-simple-server.nix
+  ./services/networking/url-bot-rs.nix
   ./services/networking/v2ray.nix
   ./services/networking/vsftpd.nix
   ./services/networking/wasabibackend.nix
