7465b37396a9072d412428144f85d4d4bc9c0c0d
Ed Cragg <ed.cragg@eipi.xyz>
2020-11-30 20:23:47 +0000

    url-bot-rs: update module to make feature-complete
    Add features previously not covered:
    
     - Connection to multiple IRC nets/servers
     - Selectable runtime features
     - Plugins and API keys
     - Database file handling for each network
     - Status channels

--- nixos/modules/services/networking/url-bot-rs.nix
+++ nixos/modules/services/networking/url-bot-rs.nix
@@ -4,9 +4,156 @@ with lib;
 
 let
   cfg = config.services.url-bot-rs;
-
   format = pkgs.formats.toml {};
-  configFile = format.generate "url-bot-rs.toml" cfg.settings;
+
+  configFiles = mapAttrsToList (name: val: format.generate "${name}.toml" {
+    connection = {
+      nickname = val.nickname;
+      alt_nicks = val.altNicks;
+      server = val.server;
+      password = val.password;
+      port = val.port;
+      use_ssl = val.ssl;
+      channels = val.channels;
+      nick_password = val.nickPassword;
+    };
+    features = genAttrs val.features (name: true);
+    parameters = { status_channels = val.statusChannels; };
+    database = if val.sqliteDb then { type = "sqlite"; path = "/var/lib/url-bot-rs/${name}.db"; } else {};
+    network = { name = "${name}"; enable = val.enable; };
+    plugins = {
+      youtube = { api_key = val.youtubeAPIKey; };
+      imgur = { api_key = val.imgurAPIKey; };
+    };
+  }) cfg.networks;
+
+  serverOpts = { name, ... }: {
+    options = {
+      enable = mkOption {
+        type = types.bool;
+        default = true;
+        description = ''
+          Whether to enable connection to this IRC network.
+        '';
+      };
+
+      nickname = mkOption {
+        type = types.str;
+        default = "url-bot-rs";
+        example = "urlify";
+        description = ''
+          Primary nickname of the bot on IRC.
+        '';
+      };
+
+      altNicks = mkOption {
+        type = types.listOf types.str;
+        default = [];
+        example = ["url-bot-rs_" "url-bot-rs__"];
+        description = ''
+          List of alternative nicks to use if the primary nick is taken.
+        '';
+      };
+
+      nickPassword = mkOption {
+        type = types.str;
+        default = "";
+        description = ''
+          Password for the primary nick.
+        '';
+      };
+
+      server = mkOption {
+        type = types.str;
+        example = "irc.example.com";
+        description = ''
+          Hostname of the IRC server to connect to.
+        '';
+      };
+
+      password = mkOption {
+        type = types.str;
+        default = "";
+        description = ''
+          Password for the IRC server.
+        '';
+      };
+
+      port = mkOption {
+        type = types.port;
+        default = 6667;
+        example = 6697;
+        description = ''
+          Port of the IRC server to connect to.
+        '';
+      };
+
+      ssl = mkOption {
+        type = types.bool;
+        default = false;
+        description = ''
+          Whether to enable SSL/TLS on the IRC connection.
+        '';
+      };
+
+      channels = mkOption {
+        type = types.listOf types.str;
+        default = [];
+        example = ["#nixos"];
+        description = ''
+          List of channels the bot joins on connect.
+        '';
+      };
+
+      features = mkOption {
+        type = types.listOf types.str;
+        default = [];
+        example = [ "history" "reconnect" "report_metadata" ];
+        description = ''
+          List of bot features to enable.
+        '';
+      };
+
+      statusChannels = mkOption {
+        type = types.listOf types.str;
+        default = [];
+        example = [ "#url-bot-rs-status" ];
+        description = ''
+          List of channels to post status information to.
+        '';
+      };
+
+      sqliteDb = mkOption {
+        type = types.bool;
+        default = false;
+        description = ''
+          Whether to enable sqlite database and pre-post information.
+          Note that you must also enable the <literal>history</literal>
+          feature for this to work.
+        '';
+      };
+
+      youtubeAPIKey = mkOption {
+        type = types.str;
+        default = "";
+        description = ''
+          YouTube API key to use for title retrieval. Adding the API key here
+          will enable the YouTube plugin automatically.
+        '';
+      };
+
+      imgurAPIKey = mkOption {
+        type = types.str;
+        default = "";
+        description = ''
+          Imgur API key to use for title retrieval. Adding the API key here
+          will enable the Imgur plugin automatically.
+        '';
+      };
+
+    };
+  };
+
 in
 {
   options.services.url-bot-rs = {
@@ -21,78 +168,32 @@ in
       '';
     };
 
-    settings = mkOption {
-      type = format.type;
-      default = {};
-      description = ''
-        Configuration for url-bot-rs, see https://github.com/nuxeh/url-bot-rs#configuration-file-options
-        for supported values.
-      '';
-    };
+    networks = mkOption {
+        type = with types; attrsOf (submodule serverOpts);
+        description = "Attribute set of servers to connect to, and their configuration options";
+        example = ''
+          "examplenet" = {
+            nickname = "urlify";
+            server = "irc.example.com";
+            port = "6697";
+            ssl = true;
+            channels = [ "#nixos" ];
+            features = [ "history" "reconnect" "report_metadata" ];
+            sqliteDb = true;
+          }
+        '';
+      };
 
-    nickname = mkOption {
-      type = types.str;
-      default = "url-bot-rs";
-      example = "urlify";
-      description = ''
-        Primary nickname of the bot on IRC.
-      '';
-    };
-
-    server = mkOption {
-      type = types.str;
-      example = "irc.example.com";
-      description = ''
-        Hostname of the IRC server to connect to.
-      '';
-    };
-
-    port = mkOption {
-      type = types.port;
-      default = 6667;
-      example = 6697;
-      description = ''
-        Port of the IRC server to connect to.
-      '';
-    };
-
-    ssl = mkOption {
-      type = types.bool;
-      default = cfg.port == 6697;
-      description = ''
-        Whether to enable SSL/TLS on the IRC connection.
-      '';
-    };
-
-    channels = mkOption {
-      type = types.listOf types.str;
-      default = [];
-      example = ["#nixos"];
-      description = ''
-        List of channels the bot joins on connect.
-      '';
-    };
   };
 
   config = mkIf cfg.enable {
-    services.url-bot-rs.settings = {
-      connection = mapAttrs (name: mkDefault) {
-        # These are the basic settings required to get the bot connected and joined.
-        nickname = cfg.nickname;
-        server = cfg.server;
-        port = cfg.port;
-        use_ssl = cfg.ssl;
-        channels = cfg.channels;
-      };
-    };
-
     systemd.services.url-bot-rs = {
       description = "Minimal IRC URL bot in Rust";
       after = [ "network.target" ];
       wantedBy = [ "multi-user.target" ];
 
       serviceConfig = {
-        ExecStart = "${pkgs.url-bot-rs}/bin/url-bot-rs -c ${configFile} " + concatStringsSep " " cfg.extraArgs;
+        ExecStart = "${pkgs.url-bot-rs}/bin/url-bot-rs -c " + concatStringsSep " -c " configFiles + concatStringsSep " " cfg.extraArgs;
         DynamicUser = true;
         Restart = "always";
         RestartSec = 10;
